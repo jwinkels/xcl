24eb1bfc4351a0012221d2b24717da37c3a77a8e
Winkels <Jan.Winkels@opitz-consulting.com>
2020-03-18 08:58:42 +0100

    load instantclient post install

--- scripts/post-install.js
+++ scripts/post-install.js
@@ -1,6 +1,10 @@
 const fs = require('fs-extra');
 const homedir = require('os').homedir();
 const xclHome = homedir + '/AppData/Roaming/xcl';
+const request = require ("request");
+var zip = require ('adm-zip');
+var os = require ('os');
+
 
 if (!fs.existsSync(xclHome)) {
   fs.mkdirSync(xclHome);
@@ -9,3 +13,34 @@ if (!fs.existsSync(xclHome)) {
 fs.copySync('./scripts/artifacts/software.yml', xclHome + '/software.yml');
 fs.closeSync(fs.openSync(xclHome + '/projects.yml', 'w'));
 fs.closeSync(fs.openSync(xclHome + '/local.yml', 'w'));
+
+var options = {};
+
+var setPath = "";
+
+if (os.platform() === 'win32'){
+  options.uri = "https://download.oracle.com/otn_software/nt/instantclient/19500/instantclient-basic-windows.x64-19.5.0.0.0dbru.zip";
+  
+}
+
+if (os.platform() === 'linux'){
+  options.uri = "https://download.oracle.com/otn_software/linux/instantclient/19600/instantclient-basic-linux.x64-19.6.0.0.0dbru.zip";
+}
+
+options.headers = {};
+
+var filename = xclHome + '/instantclient-basic.zip';
+
+
+request(options)
+    .pipe(
+      fs.createWriteStream(filename)
+          .on('close', function(){
+            var ic = new zip(filename);
+            ic.extractAllTo(xclHome+'/');
+            if (os.platform()==='win32'){
+              console.log(os.userInfo());
+              setPath = 'setx path "%path%";'+xclHome+'/'+ic.getEntries()[0].entryName.toString();
+            }
+          })
+      );
