7247df2137a66627d405416d8e98b023ce17c44c
Winkels <Jan.Winkels@opitz-consulting.com>
2020-03-23 13:58:45 +0100

    oauth complete

--- src/lib/GithubCredentials.ts
+++ src/lib/GithubCredentials.ts
@@ -1,7 +1,7 @@
 import * as os from "os";
 import * as yaml from "yaml";
 import * as fs from "fs-extra";
-import { Server } from 'http';
+import * as http from 'http';
 import * as Oauth from 'client-oauth2';
 import * as Express from 'express';
 import { resolve } from 'dns';
@@ -38,39 +38,48 @@ export class GithubCredentials{
         
        var githubAuth = new Oauth({
         clientId: '3c1dea5b9bd633b11c8b',
-        clientSecret: 'a800d53588efa396ffe097eceed7a21e1dead7b0',
         accessTokenUri: 'https://github.com/login/oauth/access_token',
         authorizationUri: 'https://github.com/login/oauth/authorize',
         redirectUri: 'http://localhost:9999/auth/github/callback'
        });
 
-       let app = Express();
-
-       app.get('/auth/github', function (req, res) {
-        var uri = githubAuth.code.getUri()
        
-        res.redirect(uri+'&login='+username)
-      });
 
+     let app = Express();
+
+     let redirectServer=app.get('/auth/github', function (req, res) {
+        var uri = githubAuth.code.getUri();
+        res.redirect( uri + '&login='+username );
+        redirectServer.close();
+      }).listen(9998);
      
       
-      
-      let server=app.get('/auth/github/callback', function (req, res) {
-        githubAuth.code.getToken(req.originalUrl)
-          .then(function (user) {
-            // Refresh the current users access token.
-            //user.refresh();
-            //console.log(user.accessToken);
-            var credentials = GithubCredentials.readCredentials();
-            credentials.github=user.accessToken;
-            fs.writeFileSync(GithubCredentials.xclHome + "/" + "credentials.yml", yaml.stringify(credentials));
-            // We should store the token into a database.
-            return res.send(user.accessToken)
-          })
-        .finally(()=>{
-            server.close()
-        });
+      let server=app.get('/auth/github/callback', function (req, express) {
+            let authToken="";
+            let httpReq=http.request('http://130.61.153.127'+req.originalUrl,(res)=>{
+                res.on('data', (d) => {
+                    authToken += d;
+                });
+            
+                res.on('end',()=>{
+                    var credentials = GithubCredentials.readCredentials();
+                    credentials.github=authToken;
+                    fs.writeFileSync(GithubCredentials.xclHome + "/" + "credentials.yml", yaml.stringify(credentials));
+                });
+            
+                }).on('error', (e) => {
+                    console.error(e);
+                }).on('finish',()=>{
+                    express.status(200).send("Authenticated! XCL can be used now!");
+                    server.close();
+                });
+            
+                httpReq.on('error', (e) => {
+                    console.error(e);
+                });
+                
+                httpReq.end();
       }).listen(9999);
-      open(githubAuth.code.getUri());
+      open('http://localhost:9998/auth/github');
     }
 }
