7c53188608b813ebf7d22e356c7c440ed1cc3432
Maik Michel <maik.michel78@gmail.com>
2021-06-09 17:38:32 +0200

    Fix termout message

--- src/lib/scripts/create_xcl_users.sql
+++ src/lib/scripts/create_xcl_users.sql
@@ -10,9 +10,9 @@ define _logic_schema =^4
 define _app_schema =^5
 
 column script_name NEW_VALUE v_script_name
-set termout off --hide this from the user
-select case to_number(substr(version, 1, instr(version, '.', 1, 1)-1)) 
-        when 11 then 
+set termout off
+select case to_number(substr(version, 1, instr(version, '.', 1, 1)-1))
+        when 11 then
           'create_schema_user_11.sql'
         else
           'create_schema_user.sql'
