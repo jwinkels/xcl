5e882ba783196a2ff6692d788f83c8925c4c5a01
jaw <Jan.Winkels@opitz-consulting.com>
2021-06-15 13:57:27 +0200

    bug in workspace detection

--- src/lib/scripts/create_workspace.sql
+++ src/lib/scripts/create_workspace.sql
@@ -12,7 +12,7 @@ BEGIN
     select count(*)
     into v_workspace_exists
     from apex_workspaces
-    where workspace = '^1';
+    where workspace = upper('^1');
 
     dbms_output.put_line('create workspace ^1 and primary_schema ^2');
 
