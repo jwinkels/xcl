e9d9bc5f1ae9f2c7ed4b5bffff6ef1461b95742a
Maik Michel <maik.michel78@gmail.com>
2021-07-22 16:47:57 +0200

    ungesehen

--- src/lib/DeliveryMethod.ts
+++ src/lib/DeliveryMethod.ts
@@ -2,7 +2,7 @@ import { ProjectFeature } from './ProjectFeature';
 
 export interface DeliveryMethod{
     install(feature:ProjectFeature, projectPath:string, singleSchema:boolean)   : void;
-    build(projectName:string, version:string, mode:string )     : void;    
+    build(projectName:string, version:string, mode:string, commit:string|undefined)     : void;
     deploy(projectName:string, connection:string, password:string, schemaOnly:boolean, ords:string, silentMode:boolean, version:string, mode:string, schema:string|undefined) : void;
   //  remove()    : void;
 }
