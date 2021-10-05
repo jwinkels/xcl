13a55f37d7be58e46ee353d6b520911349571bac
jaw <Jan.Winkels@opitz-consulting.com>
2021-10-05 12:10:12 +0200

    removed console log

--- src/lib/Utils.ts
+++ src/lib/Utils.ts
@@ -8,7 +8,6 @@ export class Utils{
      * Given a path the function searches for spaces and enwraps it in quotes when necessary
      */
     static checkPathForSpaces(path:string):string {
-        console.log('before',path);
         if (path.includes(' ')){
             return this.enwrapInQuotes(path);
         }else{
