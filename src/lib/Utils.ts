270b80a35f603c763d3ff4fca721a7f0c820c970
Maik Michel <maik.michel78@gmail.com>
2021-11-18 09:11:44 +0100

    addLine to gitignore

--- src/lib/Utils.ts
+++ src/lib/Utils.ts
@@ -1,10 +1,13 @@
+import { existsSync, readFileSync, writeFileSync } from "fs-extra"
+import { EOL } from "os";
+
 export class Utils{
     static enwrapInQuotes(aString:string):string {
         return '"'+aString+'"';
     }
 
     /**
-     * 
+     *
      * Given a path the function searches for spaces and enwraps it in quotes when necessary
      */
     static checkPathForSpaces(path:string):string {
@@ -14,4 +17,28 @@ export class Utils{
             return path;
         }
     }
+
+    // read .env file & convert to array
+    static getLinesFromFile = (file:string) => {
+        if (existsSync(file)) {
+            return readFileSync(file, "utf-8").split(EOL);
+    } else {
+            return [];
+        }
+    };
+
+    static addLine = (file: string, content: string) => {
+        const lines = Utils.getLinesFromFile(file);
+        const targetLine = lines.find((line: string) => line === content);
+        if (targetLine !== undefined) {
+          // update existing line
+          const targetLineIndex = lines.indexOf(targetLine);
+          lines.splice(targetLineIndex, 1, `${content}`);
+        } else {
+          // create new line
+          lines.push(`${content}`);
+        }
+        // write everything back to the file system
+        writeFileSync(file, lines.join(EOL));
+      };
 }
