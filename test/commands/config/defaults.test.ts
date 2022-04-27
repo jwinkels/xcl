420a9bd661707db9fad760d7c40e651638049278
jaw <Jan.Winkels@opitz-consulting.com>
2022-04-27 16:58:50 +0200

    new tests

--- test/commands/config/defaults.test.ts
+++ test/commands/config/defaults.test.ts
@@ -1,17 +1,28 @@
 import {expect, test} from '@oclif/test'
 
-describe('config:defaults', () => {
-  test
-  .stdout()
-  .command(['config:defaults'])
-  .it('runs hello', ctx => {
-    expect(ctx.stdout).to.contain('hello world')
-  })
+describe('xcl config defaults --list', () => {
+   test
+   .stdout()
+   .command(['config defaults', '--list'])
+   .it('shows global configuration variables', ctx => {
+      expect(ctx.stdout).not.empty;
+      expect(ctx.stdout).contains('Global');
+   });
 
-  test
-  .stdout()
-  .command(['config:defaults', '--name', 'jeff'])
-  .it('runs hello --name jeff', ctx => {
-    expect(ctx.stdout).to.contain('hello jeff')
-  })
-})
+   test
+   .stdout()
+   .command(['config defaults', 'project', '--list'])
+   .it('shows configuration variables of project', ctx => {
+      expect(ctx.stdout).not.empty;
+      expect(ctx.stdout).not.contains('Global');
+   });
+});
+
+describe('xcl config defaults set variable', () => {
+   test
+      .stdout()
+      .command(['config defaults', 'connection', '192.168.99.101:1521/XEPDB1', 'test'])
+      .it('set default variable to 192.168.99.101:1521/XEPDB1', ctx =>{
+         expect(ctx.stdout).contains('OK');
+      });
+});
